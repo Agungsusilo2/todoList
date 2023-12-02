1dc23a2aa914edab6daa2d97dece36e85189c4fa
Agungsusilo2 <105635993+Agungsusilo2@users.noreply.github.com>
2023-12-02 16:28:02 +0700

    TodoListService.java

--- src/Service/TodoListService.java
+++ src/Service/TodoListService.java
@@ -1,11 +1,12 @@
 package Service;
 
-import Domain.Categories;
 import Entity.TodoList;
+
+import java.util.UUID;
 
 public interface TodoListService {
     void ShowTodoListService();
     public void AddTodoListService(TodoList todoList);
     boolean RemoveTodoListService(Integer number);
-
+    boolean UpdateTodoListService(UUID number, TodoList updateTodoList);
 }

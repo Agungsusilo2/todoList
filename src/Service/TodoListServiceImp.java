d0355d48db1879008efb89cafc55a9195198444c
Agungsusilo2 <105635993+Agungsusilo2@users.noreply.github.com>
2023-12-02 16:28:25 +0700

    TodoListServiceImp.java

--- src/Service/TodoListServiceImp.java
+++ src/Service/TodoListServiceImp.java
@@ -3,8 +3,10 @@ package Service;
 import Entity.TodoList;
 import Repository.TodoListRepositoryImp;
 
+import java.util.UUID;
 
-public class TodoListServiceImp implements TodoListService{
+
+public class TodoListServiceImp implements TodoListService {
     private TodoListRepositoryImp todoListRepositoryImp;
 
     public TodoListServiceImp(TodoListRepositoryImp todoListRepositoryImp) {
@@ -15,10 +17,11 @@ public class TodoListServiceImp implements TodoListService{
     public void ShowTodoListService() {
         System.out.println("TODO LIST");
         Integer i = 0;
-        for (var todo : this.todoListRepositoryImp.findAll()){
-            if(todo !=null){
-                System.out.println(i+1 +". "+"Todo : "+todo.getAddTask() +" Description : "+todo.getDescription()
-                +" dueDate : "+todo.getDueDate().toString() +" Categories : "+ todo.getCategories().toString());
+        for (var todo : this.todoListRepositoryImp.findAll()) {
+            if (todo != null) {
+                System.out.println(i + 1 + ". " + " No Identity " + todo.getNoIdentity() + " Todo : " + todo.getAddTask() +
+                        " Description : " + todo.getDescription()
+                        + " dueDate : " + todo.getDueDate().toString() + " Categories : " + todo.getCategories().toString());
                 i++;
             }
         }
@@ -31,9 +34,19 @@ public class TodoListServiceImp implements TodoListService{
 
     @Override
     public boolean RemoveTodoListService(Integer number) {
-        if(this.todoListRepositoryImp.delete(number)){
+        if (this.todoListRepositoryImp.delete(number)) {
             System.out.println("Sukses menghapus TODO");
-        }else{
+        } else {
+            System.out.println("Gagal Menghapus TODO");
+        }
+        return true;
+    }
+
+    @Override
+    public boolean UpdateTodoListService(UUID number, TodoList updateTodoList) {
+        if (this.todoListRepositoryImp.update(number, updateTodoList)) {
+            System.out.println("Sukses mengupdate TODO");
+        } else {
             System.out.println("Gagal Menghapus TODO");
         }
         return true;

6e893a1bc2e5d55bb7f080fadcebbb5c6530c7ce
Agungsusilo2 <105635993+Agungsusilo2@users.noreply.github.com>
2023-12-02 16:27:04 +0700

    TodoListRepository.java

--- src/Repository/TodoListRepository.java
+++ src/Repository/TodoListRepository.java
@@ -2,9 +2,12 @@ package Repository;
 
 import Entity.TodoList;
 
+import java.util.UUID;
+
 public interface TodoListRepository {
     void save(TodoList todoList);
     boolean delete(Integer number);
     TodoList[] findAll();
+    boolean update(UUID number, TodoList updateTodoList);
 }
 
